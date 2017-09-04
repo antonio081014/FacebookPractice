20cb46eb8ee487184304f5968d19f655e120fffc
Antonio081014 <antonio081014@antonio081014.com>
2017-09-04 16:19:49 -0700

    Initial commit.

---
+++ FacebookPractice/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FacebookPractice
+//
+//  Created by Antonio081014 on 9/4/17.
+//  Copyright © 2017 sample.com. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
