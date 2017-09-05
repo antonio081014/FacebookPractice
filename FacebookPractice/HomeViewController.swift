c8123d442e791d5ec874c830eb1c6658104b4a45
Antonio081014 <antonio081014@antonio081014.com>
2017-09-04 17:25:45 -0700

    Add FeedCell

--- FacebookPractice/HomeViewController.swift
+++ FacebookPractice/HomeViewController.swift
@@ -10,11 +10,72 @@ import UIKit
 
 class HomeViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    private let cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.title = "Facebook Feed"
-        self.collectionView?.backgroundColor = .red
+        
+        self.collectionView?.alwaysBounceVertical = true
+        self.collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        self.collectionView?.register(FeedCell.self, forCellWithReuseIdentifier: cellID)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.width, height: 50)
     }
 }
 
+class FeedCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "Sample News"
+        return label
+    }()
+    
+    let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = .red
+        return iv
+    }()
+    
+    func setupViews() {
+        self.backgroundColor = .white
+        self.addSubview(self.nameLabel)
+        self.addSubview(self.profileImageView)
+        
+        self.profileImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+        self.profileImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+        self.profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        self.profileImageView.widthAnchor.constraint(equalTo: self.profileImageView.heightAnchor, constant: 0).isActive = true
+        
+        self.nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 0).isActive = true
+        self.nameLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0).isActive = true
+        self.nameLabel.leftAnchor.constraint(equalTo: self.profileImageView.rightAnchor, constant: 8).isActive = true
+        self.nameLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        
+    }
+}
